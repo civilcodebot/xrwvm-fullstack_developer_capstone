b286ba0357ef23ad6aa7ca56212287ac105de1f4
civilcodebot <lk9048+git@gmail.com>
2025-11-15 10:50:09 +0800

    module 3

--- server/database/app.js
+++ server/database/app.js
@@ -59,16 +59,38 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
 //Write your code here
+  try {
+    const dealer = await Dealerships.find();
+    res.json(dealer);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching documents' });
+    }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+  try{
+    const state = req.params.state.toUpperCase();
+    const dealers = await Dealerships.find({state});
+    res.json(dealers);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
+  try {
+    const dealerId = req.params.id;
+    const dealer = await Dealerships.find({id: dealerId});
+    if (!dealer) {
+      res.status(404).json({ error: 'Dealership not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 //Express route to insert review
